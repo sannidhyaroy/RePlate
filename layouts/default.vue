f65a600f3909daf05a8f7910853f06c3c82639dd
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2025-05-27 00:03:13 +0530

    `feat`: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="default-layout">
+        <header>
+            <NuxtLink to="/" class="logo">
+                <img src="../public/favicon.ico" alt="Logo">
+            </NuxtLink>
+            <NavigationBar />
+        </header>
+        <slot />
+    </div>
+</template>
