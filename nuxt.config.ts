82d4388832fc819ce9a6fac95f6b86d7a505b317
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2025-05-27 02:20:41 +0530

    `feat`: add tailwind and nuxtui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/fonts',
-    '@nuxt/eslint'
-  ]
-})
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@nuxt/eslint",
+  ],
+  css: ["~/assets/css/main.css"],
+});
