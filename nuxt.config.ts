032178c18411de8f6db540a6c9a6a321fb168a17
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2025-05-27 05:54:09 +0530

    `feat(auth)`: add auth callback page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
   ],
   css: ["~/assets/css/main.css"],
   supabase: {
+    redirect: true,
     redirectOptions: {
       login: "/login",
       callback: "/confirm",
