a1920aefed7cfa6ec97c2e737dc73a88f467c49e
Anxhul10 <anxhul10@gmail.com>
2025-05-27 15:01:09 +0530

    refactor: restructured NavigationBar and default.vue with divs and flex

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,19 +1,24 @@
 <template>
     <nav class="navigation-bar">
-        <ul>
-            <li class="home-link">
-                <NuxtLink to="/">Home</NuxtLink>
-            </li>
-            <li v-if="!user" class="login-link">
-                <NuxtLink to="/login">Login</NuxtLink>
-            </li>
-            <li v-if="!user" class="signup-link">
-                <NuxtLink to="/signup">Sign Up</NuxtLink>
-            </li>
-            <li v-else class="logout-link">
-                <NuxtLink to="/logout">Log Out</NuxtLink>
-            </li>
-        </ul>
+            <div style="display: flex; justify-content: start; align-items: center; margin:10px">
+                <div style="margin-right: 30px">
+                    <NuxtLink to="/">Replate</NuxtLink>
+                </div>
+                <div style="margin-right:30px">
+                    <UInput placeholder="Search Recepies ..." />
+                </div>
+                <div v-if="!user" class="login-link">
+                    <NuxtLink to="/login">Login</NuxtLink>
+                </div>
+                <div>
+                    <li  v-if="!user" class="signup-link">
+                        <NuxtLink to="/signup">Sign Up</NuxtLink>
+                    </li>
+                    <li v-else class="logout-link">
+                        <NuxtLink to="/logout">Log Out</NuxtLink>
+                    </li>
+                </div>    
+            </div>
     </nav>
 </template>
 
