9bf60523facfc635c90b03e3b6ee0a7d2cee322a
Anxhul10 <anxhul10@gmail.com>
2025-05-27 15:52:53 +0530

    fix: update auth buttons layout with flex for better alignment

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -2,21 +2,23 @@
     <nav class="navigation-bar">
             <div style="display: flex; justify-content: start; align-items: center; margin:10px">
                 <div style="margin-right: 30px">
-                    <NuxtLink to="/">Replate</NuxtLink>
+                    <NuxtLink to="/">Home</NuxtLink>
                 </div>
                 <div style="margin-right:30px">
                     <UInput placeholder="Search Recepies ..." />
                 </div>
-                <div v-if="!user" class="login-link">
-                    <NuxtLink to="/login">Login</NuxtLink>
+
+                <!-- auth buttons-->
+                <div v-if="!user" style="display:flex;">
+                    <div class="login-link" style="margin-right: 10px">
+                        <NuxtLink to="/login">Login</NuxtLink>
+                    </div>
+                    <div class="signup-link">
+                        <NuxtLink to="/signup">Sign Up</NuxtLink>                        
+                    </div>
                 </div>
-                <div>
-                    <li  v-if="!user" class="signup-link">
-                        <NuxtLink to="/signup">Sign Up</NuxtLink>
-                    </li>
-                    <li v-else class="logout-link">
+                <div v-else class="logout-link">
                         <NuxtLink to="/logout">Log Out</NuxtLink>
-                    </li>
                 </div>    
             </div>
     </nav>
@@ -25,5 +27,5 @@
 <script lang="ts" setup>
 const user = useSupabaseUser();
 </script>
-
-<style></style>
+<style>
+</style>
