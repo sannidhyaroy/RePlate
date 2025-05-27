6cc82158a5b068b4a9e56b91e2846e1c63008131
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2025-05-27 05:46:21 +0530

    `feat(auth)`: add logout functionality

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,21 +1,24 @@
 <template>
     <nav class="navigation-bar">
         <ul>
-            <li>
+            <li class="home-link">
                 <NuxtLink to="/">Home</NuxtLink>
             </li>
-            <li>
+            <li v-if="!user" class="login-link">
                 <NuxtLink to="/login">Login</NuxtLink>
             </li>
-            <li>
+            <li v-if="!user" class="signup-link">
                 <NuxtLink to="/signup">Sign Up</NuxtLink>
+            </li>
+            <li v-else class="logout-link">
+                <NuxtLink to="/logout">Log Out</NuxtLink>
             </li>
         </ul>
     </nav>
 </template>
 
 <script lang="ts" setup>
-
+const user = useSupabaseUser();
 </script>
 
 <style></style>
---
+++ pages/logout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const toast = useToast()
+if (!user.value) {
+    // If the user is not logged in, redirect to the home page
+    navigateTo('/')
+} else {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+        toast.add({
+            title: 'Logout Failed',
+            description: error.message,
+            color: 'error'
+        })
+    } else {
+        toast.add({
+            title: 'Away You Go!',
+            description: 'You have been logged out.',
+            color: 'success'
+        })
+        // Redirect to the home page after successful logout
+        navigateTo('/')
+    }
+}
+</script>
+
+<template>
+    <div>We're logging you out...</div>
+</template>
