603c91274f60a680cc22b842c0f0ca0695bfcd81
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2025-05-27 18:53:05 +0530

    `feat`: migrate to Nuxt UI NavBar

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,44 +1,47 @@
 <template>
-    <nav class="navigation-bar">
-            <div style="display: flex; justify-content:space-between; align-items: center; margin:10px">
-                <div style="display: flex; margin-left:5px;">
-                    <div style="margin-right: 10px;">
-                        <NuxtLink to="/" class="logo">
-                        <img src="../public/favicon.ico" alt="Logo">
-                    </NuxtLink>
-                    </div>
-                    <div style="margin-right:10px">
-                        <NuxtLink to="/">Home</NuxtLink>
-                    </div>
-                    <div >
-                        <UInput placeholder="Search Recepies ..." />
-                    </div>
-                </div>
-
-                <!-- auth buttons-->
-                <div v-if="!user" style="display:flex">
-                    <div class="login-link" style="margin-right:10px">
-                          <UButton >
-                            <NuxtLink to="/login">Login</NuxtLink>
-                          </UButton>
-                    </div>
-                    <div class="signup-link">
-                        <UButton>
-                            <NuxtLink to="/signup">Sign Up</NuxtLink>                        
-                        </UButton>
-                    </div>
-                </div>
-                <div v-else class="logout-link">
-                    <UButton>
-                        <NuxtLink to="/logout">Log Out</NuxtLink>
-                    </UButton>
-                </div>    
-            </div>
-    </nav>
+    <UNavigationMenu :items="items" class="w-full justify-end" highlight />
 </template>
 
 <script lang="ts" setup>
 const user = useSupabaseUser();
+
+const authItems = computed<NavigationMenuItem>(() =>
+    !user.value ? [{
+        label: 'Sign Up',
+        to: '/signup',
+        icon: 'i-lucide-user-plus',
+    },
+    {
+        label: 'Log In',
+        to: '/login',
+        icon: 'i-lucide-log-in',
+    }] : [{
+        label: 'Log Out',
+        to: '/logout',
+        icon: 'i-lucide-log-out',
+    },
+    ]
+);
+const items = computed<NavigationMenuItem[][]>(() => [
+    [
+        {
+            label: 'Home',
+            to: '/',
+            icon: 'i-lucide-house',
+        },
+    ],
+    authItems.value,
+    [
+        {
+            label: 'GitHub',
+            icon: 'i-lucide-github',
+            to: 'https://github.com/sannidhyaroy/RePlate',
+            target: '_blank',
+        },
+    ]
+]);
+watch(user, () => {
+}, { immediate: true });
+
 </script>
-<style>
-</style>
+<style></style>
