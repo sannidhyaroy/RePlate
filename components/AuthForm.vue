043acf237f81c25fcfa12c0b22cf1b255836d803
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2025-05-27 05:42:33 +0530

    `feat(auth)`: add login functionality to auth form
    - add functions to handle user login and sign up
    - add toast notifications on success or failure
    - add loading icon to form button

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -1,37 +1,40 @@
 <template>
     <UCard variant="subtle" class="auth-card">
-    <UForm :validate="validate" :state="state" class="auth-form" @submit="onSubmit">
-        <div v-if="signupForm" class="form-item name-field">
-            <UFormField label="Name" name="name" required>
-                <UInput v-model="state.name" type="text" placeholder="Enter your name" />
-            </UFormField>
-        </div>
-        <div class="form-item email-field">
-            <UFormField label="Email" name="email" required>
-                <UInput v-model="state.email" type="email" placeholder="Enter your email ID" />
-            </UFormField>
-        </div>
-        <div class="form-item password-field">
-            <UFormField label="Password" name="password" required>
-                <UInput
-                    v-model="state.password" type="password" :placeholder="signupForm ? 'Create a new password' : 'Enter your password'" />
-            </UFormField>
-        </div>
-        <UButton
+        <UForm :validate="validate" :state="state" class="auth-form" @submit="onSubmit">
+            <div v-if="signupForm" class="form-item name-field">
+                <UFormField label="Name" name="name" required>
+                    <UInput v-model="state.name" type="text" placeholder="Enter your name" />
+                </UFormField>
+            </div>
+            <div class="form-item email-field">
+                <UFormField label="Email" name="email" required>
+                    <UInput v-model="state.email" type="email" placeholder="Enter your email ID" />
+                </UFormField>
+            </div>
+            <div class="form-item password-field">
+                <UFormField label="Password" name="password" required>
+                    <UInput
+                        v-model="state.password" type="password" :placeholder="signupForm ? 'Create a new password' : 'Enter your password'" />
+                </UFormField>
+            </div>
+            <UButton
                 class="form-button" :label="signupForm ? 'Sign Up' : 'Log In'" :icon="signupForm ? 'mdi:account-plus' : 'mdi:login'" type="submit" :loading="loading" />
-    </UForm>
+        </UForm>
     </UCard>
 </template>
 
 <script lang="ts" setup>
 import type { FormError, FormSubmitEvent } from '@nuxt/ui';
+const supabase = useSupabaseClient();
+const toast = useToast();
 
-defineProps({
+const props = defineProps({
     signupForm: {
         type: Boolean,
         default: false
     }
 });
+const loading = ref(false);
 const state = reactive({
     name: '',
     email: '',
@@ -40,9 +43,9 @@ const state = reactive({
 
 const validate = (state: { name: string | null; email: string; password: string | null; }): FormError[] => {
     const errors: FormError[] = [];
-    if (!state.name) {
+    if (props.signupForm && !state.name) {
         errors.push({ name: 'name', message: 'Name is required' });
-    } else if (state.name.length < 3) {
+    } else if (props.signupForm && state.name!.length < 3) {
         errors.push({ name: 'name', message: 'Name must be at least 3 characters long' });
     } else if (!state.email) {
         errors.push({ name: 'email', message: 'Email is required' });
@@ -56,14 +59,63 @@ const validate = (state: { name: string | null; email: string; password: string 
     return errors;
 }
 
-const toast = useToast();
 async function onSubmit(event: FormSubmitEvent<typeof state>) {
-    toast.add({
-        title: 'Success',
-        description: 'The form has been submitted successfully!',
-        color: 'success'
-    });
+    loading.value = true;
     console.log(event.data);
+    if (props.signupForm) {
+        handleSignUp();
+    } else {
+        handleLogin();
+    }
+}
+
+const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: state.email,
+        password: state.password,
+    });
+    loading.value = false;
+    if (error) {
+        toast.add({
+            title: 'Login Failed!',
+            description: error.message,
+            color: 'error'
+        });
+    } else {
+        toast.add({
+            title: 'Log In Successful!',
+            description: 'You\'re Logged In',
+            color: 'success'
+        });
+    }
+    return data;
+}
+
+const handleSignUp = async () => {
+    const { data, error } = await supabase.auth.signUp({
+        email: state.email,
+        password: state.password,
+        options: {
+            data: {
+                display_name: state.name
+            }
+        }
+    });
+    loading.value = false;
+    if (error) {
+        toast.add({
+            title: 'Sign Up Failed!',
+            description: error.message,
+            color: 'error'
+        });
+    } else {
+        toast.add({
+            title: 'Sign Up Successful!',
+            description: 'You\'re on board RePlate!',
+            color: 'success'
+        });
+    }
+    return data;
 }
 
 </script>
