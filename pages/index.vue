89f717dd6ab9cea457126367e3b0bd5882b13f94
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2025-05-27 05:52:48 +0530

    `feat`: conditionally render homepage based on auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
-    <LandingComponent title="RePlate" tagline="Remake your plate with leftovers" />
+    <DashboardComponent v-if="user" />
+    <LandingComponent v-else title="RePlate" tagline="Remake your plate with leftovers" />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const user = useSupabaseUser()
+</script>
 
 <style></style>
